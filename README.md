96042ff2c2644084e8f6733ace08cbf4e85192f4
yavuzmester <yavuzmester@gmail.com>
2016-08-25 13:30:09 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# time-graph-svg
---
+++ index.jsx
@@ -0,0 +1,188 @@
+"use strict";
+
+const React = require("react");
+const ReactDOM = require("react-dom");
+const d3 = require("d3");
+const _ = require("underscore");
+
+const TimeGraphSvg = React.createClass({
+    propTypes: {
+        title: React.PropTypes.string.isRequired,
+        yAxisTitle: React.PropTypes.string.isRequired,
+        divWidth: React.PropTypes.number.isRequired,
+        divHeight: React.PropTypes.number.isRequired,
+        svgMargin: React.PropTypes.shape({
+            left: React.PropTypes.number.isRequired,
+            right: React.PropTypes.number.isRequired,
+            top: React.PropTypes.number.isRequired,
+            bottom: React.PropTypes.number.isRequired
+        }).isRequired,
+        svgWidth: React.PropTypes.number.isRequired,
+        svgHeight: React.PropTypes.number.isRequired,
+        data: React.PropTypes.arrayOf(      //Caution: lineGen expects data to be in order
+            React.PropTypes.shape({
+                isoDate: React.PropTypes.string.isRequired,
+                value: React.PropTypes.number.isRequired,
+                groupId: React.PropTypes.string.isRequired
+            }).isRequired
+        ).isRequired,
+        logaxis: React.PropTypes.bool.isRequired,
+        groups: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                groupId: React.PropTypes.string.isRequired,
+                groupColor: React.PropTypes.string.isRequired
+            }).isRequired
+        ).isRequired,
+        yAxisTicksEnabled: React.PropTypes.bool,
+        onBrush: React.PropTypes.func
+    },
+
+    xDomain: function() {
+        const {data} = this.props;
+        return d3.extent(data, d => new Date(d.isoDate));
+    },
+
+    xRange: function() {
+        const {svgWidth} = this.props;
+        return [0, svgWidth];
+    },
+
+    xScale: function() {
+        const xDomain = this.xDomain();
+        return d3.scaleTime().domain(xDomain).range(this.xRange());
+    },
+
+    xAxis: function() {
+        const xScale = this.xScale();
+        return d3.axisBottom(xScale);
+    },
+
+    yDomain: function() {
+        const {logaxis} = this.props;
+        const {data} = this.props;
+        return [!logaxis ? 0 : 1, d3.max(data, d => d.value)];
+    },
+
+    yRange: function() {
+        const {svgHeight} = this.props;
+        return [svgHeight, 0];
+    },
+
+    yScale: function() {
+        const {logaxis} = this.props;
+        const yDomain = this.yDomain();
+        const yRange = this.yRange();
+
+        if (!logaxis) {
+            return d3.scaleLinear().domain(yDomain).range(yRange);
+        }
+        else {
+            return d3.scaleLog().domain(yDomain).range(yRange);
+        }
+    },
+
+    yAxis: function() {
+        const {yAxisTicksEnabled} = this.props;
+        const yScale = this.yScale();
+        const yAxis = d3.axisLeft(yScale);
+
+        if (yAxisTicksEnabled) {
+            return yAxis.ticks(3, ",.0s");
+        }
+        else {
+            return yAxis.ticks(() => "");
+        }
+    },
+
+    lineGen: function() {
+        const xScale = this.xScale();
+        const yScale = this.yScale();
+        return d3.line().x(d => xScale(new Date(d.isoDate))).y(d => yScale(d.value));
+    },
+
+    brushEnabled: function() {
+        const {onBrush} = this.props;
+        return _.isFunction(onBrush);
+    },
+
+    render: function() {
+        const {title, yAxisTitle, divWidth, divHeight, svgMargin, svgWidth, svgHeight} = this.props,
+            brushEnabled = this.brushEnabled();
+
+        return (
+            /* Margin convention in D3: https://gist.github.com/mbostock/3019563 */
+            <svg width={divWidth} height={divHeight}>
+                <g className="margin axis" transform={"translate(" + svgMargin.left + "," + svgMargin.top + ")"}>
+                    <g className="x axis" transform={"translate(0," + svgHeight + ")"}/>
+                    <g className="y axis" transform={"translate(0,0)"}/>
+                    {brushEnabled ? <g className="x brush"/> : ""}
+
+                    <text x="-10" y="-10">
+                        <tspan>{title}</tspan>
+                    </text>
+
+                    <text x="5" y="5">
+                        <tspan>{yAxisTitle}</tspan>
+                    </text> :
+                </g>
+            </svg>
+        );
+    },
+
+    componentDidMount: function() {
+        const brushEnabled = this.brushEnabled();
+
+        this.componentDidMountOrUpdate();
+
+        if (brushEnabled) {
+            this.createBrush();
+        }
+    },
+
+    componentDidUpdate: function() {
+        this.componentDidMountOrUpdate();
+    },
+
+    componentDidMountOrUpdate: function() {
+        const {data, groups} = this.props,
+            xAxis = this.xAxis(),
+            yAxis = this.yAxis(),
+            lineGen = this.lineGen();
+
+        const marginAxisNode = d3.select(ReactDOM.findDOMNode(this)).select("g.margin.axis"),
+            xAxisNode = marginAxisNode.select("g.x.axis"),
+            yAxisNode = marginAxisNode.select("g.y.axis");
+
+        //update axes
+        xAxisNode.call(xAxis);
+        yAxisNode.call(yAxis);
+
+        //lines
+        marginAxisNode.selectAll(".line").remove();
+        _.each(_.groupBy(data, d => d.groupId), (groupData, groupId) => {
+            const color = _.find(groups, g => g.groupId === groupId).groupColor;
+
+            if (groupData.length > 2) {
+                marginAxisNode.append("path").
+                    attr("class", "line").
+                    attr("d", lineGen(groupData)).
+                    attr("style", "stroke-width: 2px; fill: none; stroke: " + color);
+            }
+        });
+    },
+
+    createBrush: function() {
+        const {svgWidth, svgHeight, onBrush} = this.props;
+
+        const brushNode = d3.select(ReactDOM.findDOMNode(this)).select("g.x.brush");
+
+        const brush = d3.brushX();
+        brush.extent([[0, 0], [svgWidth, svgHeight]]);
+
+        brush.on("end", onBrush.bind(this));
+
+        brushNode.call(brush).selectAll("rect").attr("y", 0).attr("height", svgHeight);
+    }
+}); //end of TimeGraphSvg component def
+
+module.exports = TimeGraphSvg;
