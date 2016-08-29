9da9871a762506603f396e930f254c3d61b179fd
gokhanayhan38 <789456123aA>
2016-08-29 17:09:33 +0300

    test in progress

---
+++ demo/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const TimeGraphSvg = require("time-graph-svg");
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+const props = require('../data.json');
+
+setTimeout(() => {
+    const tgs = ReactDOM.render(React.createElement(TimeGraphSvg, props), document.getElementById("root"));
+    tgs.on("title-click", () => console.log("title-click"));
+}, 100);
